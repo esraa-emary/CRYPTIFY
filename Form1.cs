ad887241d93fc9f771d28b2c8a73dfc62dc807b5
esraa <esraaemary33@gmail.com>
2025-07-20 22:55:13 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace Encryption_and_Decryption_Application_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
