9e4fe1ffde1b38a050c925bbb5d0722bfaaaf0a1
esraa <esraaemary33@gmail.com>
2025-07-21 10:55:18 +0200

    update initial phase

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace Encryption_and_Decryption_Application_GUI
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
